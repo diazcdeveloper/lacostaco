73a14b708a3c7a3c2328438c2273440306cb04fc
diazcdeveloper <105255678+diazcdeveloper@users.noreply.github.com>
2023-10-16 16:00:26 -0500

    first

---
+++ src/app/faqs/page.jsx
@@ -0,0 +1,6 @@
+
+export default function Faqs() {
+  return (
+    <div>Faqs</div>
+  )
+}
---
+++ src/app/support-and-partners/page.jsx
@@ -0,0 +1,6 @@
+
+export default function SuportAndPartners() {
+  return (
+    <div>SuportAndPartners</div>
+  )
+}
